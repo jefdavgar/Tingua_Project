dedcc2646fb5206d3f88ec625d4fb6c62d5bfbeb
Mafesita23 <116750999+Mafesita23@users.noreply.github.com>
2022-12-05 15:27:22 -0500

    agregar boton de eliminados en la lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,26 +13,43 @@ function agregarTarea(tarea){
                         </div>
                      </div>         `
     ul.insertAdjacentHTML("beforeend", elemento)
-    
-
-}
-addBtn.addEventListener("click", (e)=>{
-    const tarea =input.value
-    if(tarea){
-        agregarTarea(tarea)
     }
-    input.value=""
+addBtn.addEventListener("click", (e)=>{
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const tarea =input.value
+  
+  if (tarea) {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = tarea;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value = "";
+  }
 })
 document.addEventListener("keyup", function(event){
     if(event.key=="Enter"){
-        const tarea =input.value;
         if(tarea){
         agregarTarea(tarea)
         }
     input.value=""
     }
 })
+//funcion para eliminar li, p, txt
+function addDeleteBtn() {
+  //crear una constante que 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
 
-  
+  //crear evento
+  deleteBtn.addEventListener("click", (e)=>{
 
-  
+    const item = e.target.parentElement;
+    ul.removeChild(item)
+  })
+  return deleteBtn;
+};
